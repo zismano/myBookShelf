7aba9936d039fd796cba47d8e732f7512908d7f2
Ofir Zisman <zismanofir@gmail.com>
2018-05-08 12:56:18 -0700

    Clean code

--- client/src/components/search.jsx
+++ client/src/components/search.jsx
@@ -7,7 +7,11 @@ let search = props => {
 			<input id="search" type="text" 
 				placeholder="e.g: The Catcher in the Rye"
 				ref={input => searchStr = input}></input>
-			<button id="searchButton" onClick={() => props.getBook(searchStr.value)}>Find a book</button>
+			<button 
+				id="searchButton" 
+				onClick={() => props.getBook(searchStr.value)}
+				>Find a book
+			</button>
 		</div>
 	)
 }
