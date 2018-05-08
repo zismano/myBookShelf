c168295c95448cdd6579aad5029fff67db86fe50
Ofir Zisman <zismanofir@gmail.com>
2018-05-08 10:50:48 -0700

    Add MyBookList component

---
+++ client/src/components/MyBookList.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+let MyBookList = props => {
+	return (
+			<div className="shelf">
+			  <ul>
+				<li>The Catcher in the Rye</li>
+				<li>1984</li>
+				<li>Girl, Gone</li>
+			  </ul>		
+			</div>		
+	)
+}
+
+export default MyBookList;
