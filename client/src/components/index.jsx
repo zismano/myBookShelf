7e3072728b8731355e05cf4be8665307473fa07f
Ofir Zisman <zismanofir@gmail.com>
2018-05-08 11:15:01 -0700

    Give state of myBooks to MyBookList component

--- client/src/components/index.jsx
+++ client/src/components/index.jsx
@@ -3,23 +3,20 @@ import ReactDOM from 'react-dom';
 import MyBookList from './MyBookList.jsx';
 
 class App extends React.Component {
-	// constructor() {
-	// 	super();
-	// }
+	constructor() {
+		super();
+		this.state = {
+			myBooks: ['The Catcher in the Rye', '1984', 'Girl, Gone'],
+			savedBooks: ['Needful things', 'Trilogy of the century']
+		}
+	}
 
 	render() {
 	  return (
 		<div>
 			<h1>Welcome to my bookshelf</h1>
 			<p>You can search for books, add them to one of the bookshelves. Rate and comment</p>
-{/*			<div class="shelf">
-			  <ul>
-				<li>The Catcher in the Rye</li>
-				<li>1984</li>
-				<li>Girl, Gone</li>
-			  </ul>		
-			</div>*/}
-			<MyBookList />
+			<MyBookList Books={this.state.myBooks}/>
 			<div className="search">
 			  <form>
 				<input id="search" type="text" placeholder="e.g: The Catcher in the Rye"></input>
