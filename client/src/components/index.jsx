395d72aadd7242d886b34e740d3021eb0e593776
Ofir Zisman <zismanofir@gmail.com>
2018-05-08 10:37:15 -0700

    Add App component

---
+++ client/src/components/index.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+	// constructor() {
+	// 	super();
+	// }
+
+	render() {
+	  return (
+		<div>
+			<h1>Welcome to my bookshelf</h1>
+			<p>You can search for books, add them to one of the bookshelves. Rate and comment</p>
+			<div class="shelf">
+			  <ul>
+				<li>The Catcher in the Rye</li>
+				<li>1984</li>
+				<li>Girl, Gone</li>
+			  </ul>		
+			</div>
+			<div class="search">
+			  <form>
+				<input id="search" type="text" placeholder="e.g: The Catcher in the Rye"></input>
+				<button>Find a book</button>
+			  </form>
+			</div>
+		</div>
+	  )
+	}
+}
+
+ReactDOM.render(<App/>, document.getElementById("App"));
