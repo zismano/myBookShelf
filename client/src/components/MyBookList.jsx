d5ec369547bec6e3939508e77667022247c49945
Ofir Zisman <zismanofir@gmail.com>
2018-05-08 11:15:47 -0700

    Use state of myBooks instead of static sample

--- client/src/components/MyBookList.jsx
+++ client/src/components/MyBookList.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import MyBookEntry from './MyBookEntry.jsx';
 
 let MyBookList = props => {
 	return (
-			<div className="shelf">
-			  <ul>
-				<li>The Catcher in the Rye</li>
-				<li>1984</li>
-				<li>Girl, Gone</li>
-			  </ul>		
-			</div>		
+		<div className="shelf">
+		  <ul>
+		    {props.Books.map(book => 
+		    	<MyBookEntry BookEntry={book}/>
+		    )}		  	
+		  </ul>		
+		</div>		
 	)
 }
 
