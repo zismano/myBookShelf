2b61e58c5468ff6af1c77989196424c989044e10
Ofir Zisman <zismanofir@gmail.com>
2018-06-18 15:59:25 -0700

    refactor find book by title function to ES6 promise

--- server/index.js
+++ server/index.js
@@ -12,15 +12,47 @@ app.use('/', express.static(path.join(__dirname, '../client/dist')));
 
 app.get('/search', (req, res) => {
 	console.log(req.query.search);
-	https.get(`https://www.googleapis.com/books/v1/volumes?q=${req.query.search}&key=${googleKey.googleKey}`, (res) => {
-		var str = ''
-  		res.on('data', chunk => {
-    		str += chunk;
-  		});
-		res.on('end', () => {
-    		console.log(str);
-  		});
+	// https.get(`https://www.googleapis.com/books/v1/volumes?q=${req.query.search}&key=${googleKey.googleKey}`, (res) => {
+	// 	var str = ''
+  // 		res.on('data', chunk => {
+  //   		str += chunk;
+  // 		});
+	// 	res.on('end', () => {
+  //   		console.log(str);
+  // 		});
+	// })
+	findBookTitleFromAPI(req.query.search)
+	.then(books => {
+		console.log(books[0])
 	})
-});
+	.catch(err => {
+		console.log(err)})
+ })
+
+
+const findBookTitleFromAPI = title =>
+	new Promise((resolve, reject) => {
+		https.get(`https://www.googleapis.com/books/v1/volumes?q=${title}&key=${googleKey.googleKey}`, (res) => {
+			if (res.statusCode === 200) {
+				var rawData = '';
+				res.on('data', chunk => {
+					rawData += chunk;
+				});
+				res.on('end', () => {
+					try {
+						const parsedData = JSON.parse(rawData);
+						resolve(parsedData.items);
+					} catch (err) {
+						reject(err.message);
+					}
+				});
+			} else {
+				let error = new Error('Request Failed.\n' +
+										`Status Code: ${res.statusCode}`);
+				reject(error.message);
+			}
+		})
+	});
+
 
 app.listen(3000);
