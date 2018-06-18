860752d0d8cbd6188e83c6312eeccd3f6efe673e
Ofir Zisman <zismanofir@gmail.com>
2018-06-18 16:39:37 -0700

    add h1

--- client/src/components/MyBookList.jsx
+++ client/src/components/MyBookList.jsx
@@ -4,12 +4,13 @@ import MyBookEntry from './MyBookEntry.jsx';
 let MyBookList = props => {
 	return (
 		<div className="shelf">
+			<h1>Book List</h1>
 		  <ul>
-		    {props.Books.map(book => 
+		    {props.Books.map(book =>
 		    	<MyBookEntry BookEntry={book}/>
-		    )}		  	
-		  </ul>		
-		</div>		
+		    )}
+		  </ul>
+		</div>
 	)
 }
 
