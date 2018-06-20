16efa509f83a7d4621c0a4f6d27d5f78c225b3aa
Ofir Zisman <zismanofir@gmail.com>
2018-06-20 14:30:48 -0700

    refactor code from function to class

--- client/src/components/BookEntry.jsx
+++ client/src/components/BookEntry.jsx
@@ -1,17 +1,66 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-let BookEntry = props => {
-	let {
-		title,
-		author,
-		publishedDate,
-		img,
-	} = props.BookEntry;
-	return (
-		<div>
-			<li>{title} By {author}</li>
-		</div>
-	)
+class BookEntry extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			showDetails: false
+		}
+	}
+	render() {
+		let {
+			title,
+			author,
+			publishedDate,
+			thumbnail,
+		} = this.props.BookEntry;
+
+		return (
+			<div>
+				<li onClick={() =>
+						this.setState({ showDetails: !this.state.showDetails})}
+				>{title} By {author}</li>
+				{this.state.showDetails ? (
+					<div>
+						<h4>Published: {publishedDate}</h4>
+						<img src={thumbnail}></img>
+					</div>
+				) : (
+					<div></div>
+				)}
+			</div>
+		)
+	}
 }
+
+
+
+// let BookEntry = props => {
+// 	let {
+// 		title,
+// 		author,
+// 		publishedDate,
+// 		thumbnail,
+// 	} = props.BookEntry;
+// 	let showDetails = true;
+//
+// 	return (
+// 		<div>
+// 			{showDetails ? (
+// 				<div>
+// 				<li onClick={(e) => {
+// 					console.log(e);
+// 					showDetails = !showDetails}}>{title} By {author}</li>
+// 				<h4>Published: {publishedDate}</h4>
+// 				<img src={thumbnail}></img>
+// 				</div>
+// 			) : (
+// 				<li onClick={(e) => {
+// 					console.log(e);
+// 					showDetails = !showDetails}}>{title} By {author}</li>
+// 			)}
+// 		</div>
+// 	)
+// }
 
 export default BookEntry;
--- client/src/components/BookList.jsx
+++ client/src/components/BookList.jsx
@@ -1,16 +1,39 @@
-import React from 'react';
+import React, {Component} from 'react';
 import BookEntry from './BookEntry.jsx';
 
-let BookList = props => {
-	return (
-		<div className="shelf">
-		  <ul>
-		    {props.Books.map(book =>
-		    	<BookEntry BookEntry={book}/>
-		    )}
-		  </ul>
-		</div>
-	)
+class BookList extends Component {
+	constructor(props) {
+		super(props);
+	}
+	render() {
+		return (
+			<div className="shelf">
+			  <ul>
+			    {this.props.Books.map(book =>
+			    	<BookEntry
+							BookEntry={book}
+						/>
+			    )}
+			  </ul>
+			</div>
+		)
+	}
 }
+
+//
+//
+// let BookList = props => {
+// 	return (
+// 		<div className="shelf">
+// 		  <ul>
+// 		    {props.Books.map(book =>
+// 		    	<BookEntry
+// 						BookEntry={book}
+// 					/>
+// 		    )}
+// 		  </ul>
+// 		</div>
+// 	)
+// }
 
 export default BookList;
