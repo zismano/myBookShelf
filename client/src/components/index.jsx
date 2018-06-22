9646c1625b16c1b4283d43f239897e3c1d57932f
Ofir Zisman <zismanofir@gmail.com>
2018-06-21 20:56:51 -0700

    wrap render with Provider block

--- client/src/components/index.jsx
+++ client/src/components/index.jsx
@@ -1,46 +1,21 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+
+import {createBookObject}  from '../../../helpers/helpers';
+import store from '../store';
+const axios = require('axios');
 
 // components
 import BookList from './BookList.jsx';
 import Search from './search.jsx';
 import DisplaySearchedBook from './DisplaySearchedBook.jsx';
-const {createBookObject} = require('../../../helpers/helpers');
 
-const axios = require('axios');
 
 class App extends Component {
 	constructor(props) {
 	  super(props);
 	  this.state = {
-	  	// unreadBooks: [
-			// 	{
-			// 		title: "1984",
-			// 		author: "George Orwell",
-			// 		publishedDate: "1983-10-17",
-			// 		thumbnail: "http://books.google.com/books/content?id=kotPYEqx7kMC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
-			// 	},
-			// 	{
-			// 		title: "The Last Man in Europe: A Novel",
-			// 		author: "Dennis Glover",
-			// 		publishedDate: "2017-11-14",
-			// 		thumbnail: "http://books.google.com/books/content?id=E6UxDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
-			// 	}
-			// ],
-	  	// savedBooks: [
-			// 	{
-			// 		title: "1984",
-			// 		author: "George Orwell",
-			// 		publishedDate: "1983-10-17",
-			// 		thumbnail: "http://books.google.com/books/content?id=kotPYEqx7kMC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
-			// 	},
-			// 	{
-			// 		title: "The Last Man in Europe: A Novel",
-			// 		author: "Dennis Glover",
-			// 		publishedDate: "2017-11-14",
-			// 		thumbnail: "http://books.google.com/books/content?id=E6UxDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
-			// 	}
-			// ],
 			books: [
 				{
 					title: "1984",
@@ -96,37 +71,22 @@ class App extends Component {
 			searchResults,
 		} = this.state;
 
-//		let isCurrBooks = currBook !== '' ? true : false;
 	  return (
-		<div>
-			<h1>Welcome to my bookshelf</h1>
-			<p>You can search for books, add them to one of the bookshelves. Rate and comment</p>
-			<h2>My Book Shelf</h2>
-			<BookList Books={books}/>
-			<Search getBook={this.getBook}/>
-			<DisplaySearchedBook
-				searchedBookResults={searchResults}
-				addBook={this.addBookToShelf}
-			/>
-		</div>
+			<Provider store={store}>
+				<div>
+					<h1>Welcome to my bookshelf</h1>
+					<p>You can search for books, add them to one of the bookshelves. Rate and comment</p>
+					<h2>My Book Shelf</h2>
+					<BookList Books={books}/>
+					<Search getBook={this.getBook}/>
+					<DisplaySearchedBook
+						searchedBookResults={searchResults}
+						addBook={this.addBookToShelf}
+					/>
+				</div>
+			</Provider>
 	  )
 	}
 }
 
-
-			// {isCurrBooks ? (
-			// 	<div className="book-found">
-			// 			<h2>Book Found </h2>
-			// 			<h3>{currBook.title} By {currBook.authors[0]}</h3>
-			// 			{currBook.categories ? (
-			// 				<h4>Categories: {currBook.categories[0]}</h4>
-			// 			) : (
-			// 				<h4></h4>
-			// 			)}
-			// 			<h4>Published: {currBook.publishedDate}</h4>
-			// 			<img src={currBook.imageLinks.thumbnail}></img>
-			// 	</div>
-			// 		) : (
-			// 			<div>Book not found</div>
-			// 	)}
 ReactDOM.render(<App/>, document.getElementById("App"));
