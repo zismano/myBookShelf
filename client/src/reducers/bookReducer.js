f031c2df0ff194312897e52fe7a3e2894e0ff968
Ofir Zisman <zismanofir@gmail.com>
2018-06-28 16:03:17 -0700

    add reducer for add book

--- client/src/reducers/bookReducer.js
+++ client/src/reducers/bookReducer.js
@@ -2,16 +2,23 @@ import {DISPLAY_BOOKS, SEARCH_BOOK, ADD_BOOK} from '../actions/types';
 
 const initialState = {
   items: [],
-  item: {},
-  searchedItem: []  // array to render various results of searched book
+  searchResults: []  // array to render various results of searched book
 }
 
 export default function(state = initialState, action) {
   switch (action.type) {
     case SEARCH_BOOK:
-      let newState = Object.assign({}, state); // tried return {...state, searchedItem: action.payload}, doesn't work with webpack?
-      newState.searchedItem = action.payload;
+      let newState = Object.assign({}, state); // tried return {...state, searchResults: action.payload}, doesn't work with webpack?
+      newState.searchResults = action.payload;
       return newState;
+
+    case ADD_BOOK:
+      console.log('add reducer');
+      newState = Object.assign({}, state);
+      newState.items = state.items.concat([action.payload]);
+      console.log(newState);
+      return newState;
+
     default:
       return state;
   }
