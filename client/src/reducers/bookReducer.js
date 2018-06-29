9f94fd19b311f2f18c34fb10cfbca3a5462c5898
Ofir Zisman <zismanofir@gmail.com>
2018-06-28 20:21:22 -0700

    add remove book reducer

--- client/src/reducers/bookReducer.js
+++ client/src/reducers/bookReducer.js
@@ -1,21 +1,36 @@
-import {SEARCH_BOOK, ADD_BOOK} from '../actions/types';
+import {
+  SEARCH_BOOK,
+  ADD_BOOK,
+  REMOVE_BOOK
+} from '../actions/types';
+
 import {createBookObject} from '../../../helpers/helpers';
 
 const initialState = {
   items: [
     {
-      title: "1984",
-      author: "George Orwell",
-      publishedDate: "1983-10-17",
-      thumbnail: "http://books.google.com/books/content?id=kotPYEqx7kMC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
-      status: "read"
+      id: "3x9ybla",
+      volumeInfo: {
+        title: "1984",
+        authors: ["George Orwell"],
+        publishedDate: "1983-10-17",
+        imageLinks: {
+          thumbnail: "http://books.google.com/books/content?id=kotPYEqx7kMC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+        },
+        status: "read"
+      }
     },
     {
-      title: "The Last Man in Europe: A Novel",
-      author: "Dennis Glover",
-      publishedDate: "2017-11-14",
-      thumbnail: "http://books.google.com/books/content?id=E6UxDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
-      status: "unread"
+      id: "9xy3oA",
+      volumeInfo: {
+        title: ["The Last Man in Europe: A Novel"],
+        authors: ["Dennis Glover"],
+        publishedDate: "2017-11-14",
+        imageLinks: {
+          thumbnail: "http://books.google.com/books/content?id=E6UxDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+        },
+        status: "unread"
+      }
     }
   ],
   searchResults: []  // array to render various results of searched book
@@ -30,10 +45,20 @@ export default function(state = initialState, action) {
 
     case ADD_BOOK:
       newState = Object.assign({}, state);
-      let bookToAdd = createBookObject(action.payload);
-      newState.items = state.items.concat([bookToAdd]);
+  //    let bookToAdd = createBookObject(action.payload);
+  //    newState.items = state.items.concat([bookToAdd]);
+      newState.items = state.items.concat([action.payload]);
+      console.log(newState.items);
       return newState;
 
+    case REMOVE_BOOK:
+      newState = Object.assign({}, state);
+      newState.items = newState.items.filter(book => {
+        if (book.id !== action.payload.id) {
+          return book;
+        }
+      });
+      return newState;
     default:
       return state;
   }
