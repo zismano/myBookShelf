68411dbf9d8d59016c5117d0ba9c3f0c3bb40f12
Ofir Zisman <zismanofir@gmail.com>
2018-06-28 20:20:05 -0700

    add REMOVE_BOOK type

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -2,3 +2,4 @@ export const FETCH_BOOKS = 'FETCH_BOOKS';
 export const FETCH_SEARCH_RESULTS = 'FETCH_SEARCH_RESULTS';
 export const SEARCH_BOOK = 'SEARCH_BOOK';
 export const ADD_BOOK = 'ADD_BOOK';
+export const REMOVE_BOOK = 'REMOVE_BOOK';
