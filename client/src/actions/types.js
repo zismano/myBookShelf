e6dbd0f5e6bfdefab5bbb354d5323e145c492ef4
Ofir Zisman <zismanofir@gmail.com>
2018-06-28 17:26:00 -0700

    add FETCH_SEARCH_RESULT type

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,3 +1,4 @@
 export const FETCH_BOOKS = 'FETCH_BOOKS';
+export const FETCH_SEARCH_RESULTS = 'FETCH_SEARCH_RESULTS';
 export const SEARCH_BOOK = 'SEARCH_BOOK';
 export const ADD_BOOK = 'ADD_BOOK';
