dcefba6c893ba529a1a77948bf76b1a24df63a77
Ofir Zisman <zismanofir@gmail.com>
2018-07-09 13:49:42 -0700

    add AddBook component

---
+++ client/src/components/AddBook.jsx
@@ -0,0 +1,12 @@
+// import React from 'react';
+//
+// const AddBook = props =>
+//   (
+//     <div>
+//       // <button onClick={() => this.state.currBook} id="addToUnreadBooks">Add Book to Unread Book Shelf</button>
+//       // <button onClick={() => }id="addToSavedBooks">Add Book to Saved Book Shelf</button>
+//     </div>
+//   )
+//
+//
+// export default AddBook;
