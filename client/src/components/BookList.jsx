05fde955d1ce817b5c303fd602f957671defa34a
Ofir Zisman <zismanofir@gmail.com>
2018-07-09 14:16:21 -0700

    fix typo

--- client/src/components/BookList.jsx
+++ client/src/components/BookList.jsx
@@ -12,58 +12,62 @@ class BookList extends Component {
 
 	toggleBookDetails(e, index) {
 		if (e.target.value !== 'button') {	// when click on read/unread/remove don't toggle details
-			let divs = document.querySelectorAll(".extra-details");
-			divs[index].classList.toggle("invisible-div");
+			// let divs = document.querySelectorAll(".extra-details");
+			// divs[index].classList.toggle("invisible-div");
+			// change status of areDetailsShown property
 		}
 	}
 
 	render() {
+		console.log(this.props);
 		const books = this.props.books.map((book, i) => {
-			const volume = book.volumeInfo;
-			return (
-				<div className={`book-entry ${book.status}`} key={i}>
-					<li onClick={(e) => this.toggleBookDetails(e, i)}>
-						{volume.title} By {volume.authors[0]}
-						<button
-							className="read-button"
-							value="button"
-							onClick={() => this.props.changeStatusOfBook(book, 'read')}>
-							Read
-						</button>
-						<button
-							className="unread-button"
-							value="button"
-							onClick={() => this.props.changeStatusOfBook(book, 'unread')}>
-							Unread
-						</button>
-						<button
-							className="remove-button"
-							value="button"
-							onClick={() => this.props.removeBook(book)}>
-							Remove
-						</button>
-						<div className="extra-details invisible-div">
-							{volume.categories ? (
-								<h4>Categories: {volume.categories[0]}</h4>
-							) : (
-								<h4/>
-							)}
-							{volume.description ? (
-								<h4 className="book-description">{volume.description}</h4>
-							) : (
+			if (this.props.view.allBooks || (book.isRead && this.props.view.read) || (!book.isRead && this.props.view.unread)) {
+				const volume = book.volumeInfo;
+				return (
+					<div className={book.isRead ? 'book-entry read' : 'book-entry unread'} key={i}>
+						<li onClick={(e) => this.toggleBookDetails(e, i)}>
+							{volume.title} By {volume.authors[0]}
+							<button
+								className="read-button"
+								value="button"
+								onClick={() => this.props.changeStatusOfBook(book, true)}>
+								Read
+							</button>
+							<button
+								className="unread-button"
+								value="button"
+								onClick={() => this.props.changeStatusOfBook(book, false)}>
+								Unread
+							</button>
+							<button
+								className="remove-button"
+								value="button"
+								onClick={() => this.props.removeBook(book)}>
+								Remove
+							</button>
+							<div className="extra-details invisible-div">
+								{volume.categories ? (
+									<h4>Categories: {volume.categories[0]}</h4>
+								) : (
 									<h4/>
-							)}
-							{volume.averageRating && volume.ratingsCount ? (
-								<h4>Rating: {volume.averageRating}/5 ({volume.ratingsCount})</h4>
-							) : (
-								<h4/>
-							)}
-							<h4>Published: {volume.publishedDate}</h4>
-							<img src={volume.imageLinks.thumbnail}></img>
-						</div>
-					</li>
-				</div>
-			);
+								)}
+								{volume.description ? (
+									<h4 className="book-description">{volume.description}</h4>
+								) : (
+										<h4/>
+								)}
+								{volume.averageRating && volume.ratingsCount ? (
+									<h4>Rating: {volume.averageRating}/5 ({volume.ratingsCount})</h4>
+								) : (
+									<h4/>
+								)}
+								<h4>Published: {volume.publishedDate}</h4>
+								<img src={volume.imageLinks.thumbnail}></img>
+							</div>
+						</li>
+					</div>
+				);
+			}
 		})
 
 		return (
@@ -77,6 +81,7 @@ class BookList extends Component {
 }
 
 const mapStateToProps = state => ({
+	view: state.books.view,
 	books: state.books.items
 })
 
